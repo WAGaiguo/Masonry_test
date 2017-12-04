ecc0a489c220b6aeceeb5826f726501af4422bc6
wangaiguo <wangaiguo@wangaiguodeMac-mini.local>
2017-12-04 10:35:33 +0800

    Initial Commit

---
+++ 2017_12_4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  2017_12_4
+//
+//  Created by wangaiguo on 2017/12/4.
+//  Copyright © 2017年 wangaiguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 2017_12_4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  2017_12_4
+//
+//  Created by wangaiguo on 2017/12/4.
+//  Copyright © 2017年 wangaiguo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
